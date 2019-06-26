986f5110e9eae58109fbc67bf784f13abbb42b96
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-06-26 15:39:40 +0200

    Refactor serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 class CategorySerializer < ActiveModel::Serializer
-  attributes :name, :color, :ordering, :total_amount
-
-  def name
-    object.name.capitalize
-  end
-
-  def total_amount
-    object.try(:total)
-  end
+  attributes :name, :color, :ordering
 end
